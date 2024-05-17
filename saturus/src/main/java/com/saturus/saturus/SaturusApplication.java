49c4f176a85b0dc4ee559fed9bcad1678bee492f
cmblir <sodlalwl13@gmail.com>
2024-05-18 00:14:10 +0900

    passed

--- saturus/src/main/java/com/saturus/saturus/SaturusApplication.java
+++ saturus/src/main/java/com/saturus/saturus/SaturusApplication.java
@@ -2,9 +2,11 @@ package com.saturus.saturus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EntityScan
 public class SaturusApplication {
 
 	public static void main(String[] args) {
--- saturus/src/main/java/com/saturus/saturus/SpringConfig.java
+++ saturus/src/main/java/com/saturus/saturus/SpringConfig.java
@@ -4,14 +4,17 @@ import com.saturus.saturus.repository.JpaMemberRepository;
 import com.saturus.saturus.repository.MemberRepository;
 import com.saturus.saturus.service.MemberService;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EnableJpaRepositories
 public class SpringConfig {
 
-    
+    @PersistenceContext
     private final EntityManager em;
 
     @Autowired
